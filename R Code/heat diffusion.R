704bac6ecf7f1822e77b458e700f54f306cc21ec
Diego Rodriguez <poyutero@gmail.com>
2023-10-16 21:37:14 +0200

    tried R stuff, might not be feasable

--- R Code/heat diffusion.R
+++ R Code/heat diffusion.R
@@ -1,18 +1,23 @@
 # Information ----
 # Author: Diego Rodríguez
 # Date of creation: 13/10/2023
-# 
+# Last update: 16/10/2023
 
 # Loading ----
-# Load libraries
+## Load libraries ----
 require(regnet)
 require(RCy3)
 require(igraph)
 require(clusterProfiler)
 require(org.Hs.eg.db)
 require(R.utils)
+require(expm)
+require(tidyverse)
 
-# Load files
+## Load files ----
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+setwd("..")
+
 if(!file.exists("./PPI Network/9606.protein.links.v12.0.txt")){
   gunzip("./PPI Network/9606.protein.links.v12.0.txt.gz", remove=FALSE)
 }
@@ -21,14 +26,27 @@ data <- read.table(gzfile("./PPI Network/9606.protein.links.v12.0.txt"), sep=" "
 
 # Graph cleaning ----
 
-
 # Cytoscape/igraph ----
-# Connecting to Cytoscape
+## Connecting to Cytoscape ----
 
-# igraph
+## igraph ----
 graph <- graph_from_data_frame(data)
 
 # Calculate heat diffusion ----
-seed_nodes = read.csv('./PPI Network/STRING network - endometriosis IDs.csv', header = FALSE, stringsAsFactors = FALSE)
+seed_prots = read.csv('./PPI Network/STRING network - endometriosis IDs.csv', header = FALSE, stringsAsFactors = FALSE)
 
-diffusion <- seeded.diffusion(graph, seed_nodes, steps = 10)
+## Manually ----
+# h
+seed_ids = match(seed_prots$V1, as_ids(V(graph)))
+
+# D
+degs = degree(graph)
+
+# A
+adj_graph = as_adjacency_matrix(graph, sparse = FALSE)
+
+
+# Diffusion
+diffed = h*expm(-(degs-adj_graph)*0.1)
+
+rm(degs, adj_graph)
